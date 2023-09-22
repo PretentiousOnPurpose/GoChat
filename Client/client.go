a83a90f056c7753817a6c53b5e940ebcdd6becab
Yashwanth R <yash.rk@outlook.com>
2023-09-22 21:46:06 +0200

    some more house keeping

--- Client/client.go
+++ Client/client.go
@@ -2,3 +2,46 @@
 // func connect to server with chat room ID or create new room req.
 // func to chat
 // cmd processing to exit or move to another chat room or start over.
+
+package main
+
+import "fmt"
+
+func init_func() {
+	fmt.Println("---> Welcome to PretentiousOnPurpose")
+	fmt.Println("-----> Premium CHAT ROOM")
+	fmt.Println("----->")
+	fmt.Println("-------> What would you like to do?")
+	fmt.Println("---------> 1. Start a new chat room")
+	fmt.Println("---------> 2. Join an existing chat room")
+}
+
+func requestUserInput() int {
+	var usrInput int
+	var userName string
+	var chatRoomID int
+
+	fmt.Print("---------> User: ")
+	fmt.Scanln(&usrInput)
+	fmt.Print("---------> Enter your username: ")
+	fmt.Scanln(&userName)
+
+	if usrInput == 1 {
+		// createNewChatRoom(&chatRoomID)
+		chatRoomID = 100001
+	} else {
+		fmt.Print("---------> Enter the chat room ID: ")
+		fmt.Scanln(&chatRoomID)
+		// joinChatRoom(chatRoomID)
+	}
+
+	fmt.Println("------->")
+	fmt.Printf("-------> Tranferring %s to chat room ID: %d\n", userName, chatRoomID)
+
+	return 1
+}
+
+func main() {
+	init_func()
+	requestUserInput()
+}
--- Server/server.go
+++ Server/server.go
@@ -2,32 +2,7 @@ package main
 
 import "fmt"
 
-func init_func() {
-	fmt.Println("---> Welcome to PretentiousOnPurpose")
-	fmt.Println("-----> Premium CHAT ROOM")
-	fmt.Println("----->")
-	fmt.Println("-------> What would you like to do?")
-	fmt.Println("---------> 1. Start a new chat room")
-	fmt.Println("---------> 2. Join an existing chat room")
-}
-
 func handleNewConn;
-
-func requestUserInput() int {
-	var usrInput int
-	var chatRoomID int
-
-	fmt.Print("---------> User: ")
-	fmt.Scanf("%d", &usrInput)
-
-	if usrInput == 1 {
-		createNewChatRoom(&chatRoomID)
-	} else {
-		fmt.Println("---------> Enter the chat room ID: ")
-		joinChatRoom(chatRoomID)
-	}
-}
-
 func createNewChatRoom()
 func joinChatRoom()
 
