b0e011bf2b07b68d254dbd620ff0be867acc2939
Yashwanth R <yash.rk@outlook.com>
2023-10-18 22:49:00 +0200

    more updates

---
+++ web/server/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	l, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer l.Close()
+	for {
+		// Wait for a connection.
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		data, _ := io.ReadAll(conn)
+		dataStr := string(data)
+
+		fmt.Println(dataStr)
+	}
+}
--- web/static/js/index.js
+++ web/static/js/index.js
@@ -2,20 +2,49 @@
 terminal = document.querySelector(".card-content");
 terminal.scrollTop = terminal.scrollHeight;
 window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+// const WebSocket = require('ws');
 
 document.querySelector("#send").addEventListener("click", sendMsg);
 
-async function sendMsg() {
+// const socket = new WebSocket("ws://localhost:8080");
+
+connect();
+
+function connect() {
+  ws = new WebSocket('ws://localhost:8080');
+  setTimeout(bindEvents, 1000);
+  setReadyState();
+}
+
+function bindEvents() {
+  ws.onopen = function() {
+    console.log('onopen called');
+    setReadyState();
+  };
+}
+
+function setReadyState() {
+  console.log('ws.readyState: ' + ws.readyState);
+}
+
+function sendMsg() {
   termObj = document.querySelector("#terminal");
   cmdObj = document.querySelector("#cmd")
 
   if (cmdObj.innerHTML !== "") {
-    termObj.innerHTML = termObj.innerHTML + "\n<p>$: " + cmdObj.innerHTML + "</p>" 
+    if (cmdObj.innerHTML === "clear") {
+      termObj.innerHTML = "";  
+    } else {
+      termObj.innerHTML = termObj.innerHTML + "\n<p>You: " + cmdObj.innerHTML + "</p>" 
+    }
+
     cmdObj.innerHTML = "";
+    //sendMsgToLord(msg)
+    console.log("Send 1");
+    
+    ws.onopen = () => ws.send("Message");
+    
+    console.log("Send 2");
+
   }
-}
-
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
 }
