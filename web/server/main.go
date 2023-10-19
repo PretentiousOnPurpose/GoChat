079bd991048a90045840be50279ccc68583617f6
Yashwanth R <yash.rk@outlook.com>
2023-10-19 11:12:39 +0200

    POST appearing as GET to server - needs to be fixed

--- web/server/main.go
+++ web/server/main.go
@@ -1,28 +1,53 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
 	"log"
-	"net"
+	"net/http"
 )
 
-func main() {
-	l, err := net.Listen("tcp", ":8080")
+func Cors(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=ascii")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST,GET,OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,access-control-allow-origin, access-control-allow-headers")
+	w.Write([]byte("Hello, World!"))
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+
+	// fmt.Println("The Server was Pinged!")
+	// res, err := httputil.DumpRequest(r, true)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Print(string(res))
+
+	err := r.ParseForm()
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("Error parsing form")
 	}
-	defer l.Close()
-	for {
-		// Wait for a connection.
-		conn, err := l.Accept()
-		if err != nil {
-			log.Fatal(err)
-		}
+	fmt.Println(r.PostForm)
 
-		data, _ := io.ReadAll(conn)
-		dataStr := string(data)
-
-		fmt.Println(dataStr)
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	// w.Header().Set("Access-Control-Allow-Origin", "*")
+	// w.Header().Set("Access-Control-Allow-Headers", "Content-Type,access-control-allow-origin, access-control-allow-headers")
+	resp := make(map[string]string)
+	resp["message"] = "Status OK"
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		log.Fatalf("Error happened in JSON marshal. Err: %s", err)
 	}
+
+	w.Write(jsonResp)
+	return
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/plm/cors", Cors)
+	mux.HandleFunc("/data", handleRequest)
+	http.ListenAndServe(":8080", mux)
 }
--- web/static/js/index.js
+++ web/static/js/index.js
@@ -1,31 +1,10 @@
 
 terminal = document.querySelector(".card-content");
 terminal.scrollTop = terminal.scrollHeight;
-window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
-// const WebSocket = require('ws');
 
 document.querySelector("#send").addEventListener("click", sendMsg);
 
-// const socket = new WebSocket("ws://localhost:8080");
 
-connect();
-
-function connect() {
-  ws = new WebSocket('ws://localhost:8080');
-  setTimeout(bindEvents, 1000);
-  setReadyState();
-}
-
-function bindEvents() {
-  ws.onopen = function() {
-    console.log('onopen called');
-    setReadyState();
-  };
-}
-
-function setReadyState() {
-  console.log('ws.readyState: ' + ws.readyState);
-}
 
 function sendMsg() {
   termObj = document.querySelector("#terminal");
@@ -40,11 +19,33 @@ function sendMsg() {
 
     cmdObj.innerHTML = "";
     //sendMsgToLord(msg)
-    console.log("Send 1");
-    
-    ws.onopen = () => ws.send("Message");
-    
-    console.log("Send 2");
+    // fetch("http://127.0.0.1:8080/data", {
+    //   method: "POST",
+    //   body: JSON.stringify({
+    //     userId: 1,
+    //     title: "Fix my bugs",
+    //     completed: false
+    //   }),
+    //   headers: {
+    //     'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+    //     "Access-Control-Allow-Origin" : "*"
+    //   }
+    // });
+
+    $.post({
+      // type: "POST",
+      url: 'http://127.0.0.1:8080/data',
+      data:{"name":"Yash"},
+      async:true,
+      contentType: 'application/x-www-form-urlencoded;charset=UTF-8',
+      dataType : 'jsonp',   //you may use jsonp for cross origin request
+      crossDomain:true,
+      success: function(data, status, xhr) {
+        alert(xhr.getResponseHeader('Location'));
+      }
+    });
+
+    console.log("Worked");
 
   }
 }
